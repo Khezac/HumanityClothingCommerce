daabc540f07e94434c0ece66c4c2b24009f1ab80
Khezac <khezac1@gmail.com>
2024-07-25 15:47:16 -0300

    style: first banner

--- humanity-e-commerce/src/pages/Home/index.tsx
+++ humanity-e-commerce/src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/layout/Header"
+import { FirstBanner } from "../../components/HomeFirstBanner"
 import styles from "./styles.module.css"
 
 export const Home = () => {
     return (
         <main className={styles.homeContainer}>
             <Header />
+            <FirstBanner/>
         </main>
     )
 }
