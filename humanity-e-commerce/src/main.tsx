6b282f1fb60567aede8dbbbc381e16340f14b05e
Khezac <khezac1@gmail.com>
2024-07-26 15:14:42 -0300

    feat: footer finalizado

--- humanity-e-commerce/src/main.tsx
+++ humanity-e-commerce/src/main.tsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Home } from './pages/Home'
 import { Header } from './components/layout/Header'
+import { Footer } from './components/layout/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header/>
     <Home/>
+    <Footer/>
   </React.StrictMode>,
 )
