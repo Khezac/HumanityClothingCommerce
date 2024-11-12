2e4a468bd3309783ae059c0a1caed3ae5651b466
Khezac <khezac1@gmail.com>
2024-11-11 21:40:37 -0300

    fix: bordas laterais da página home em resolução FHD

--- humanity-e-commerce/src/components/layout/Footer/index.tsx
+++ humanity-e-commerce/src/components/layout/Footer/index.tsx
@@ -5,22 +5,24 @@ import LogoBranca from '../../../assets/Logos/LogoWhite.png'
 export const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.footerLinks}>
-                <a href='/'>
-                    <img src={LogoBranca} />
-                </a>
-                <div className={styles.social}>
-                    <a href="https://github.com/Khezac/" target='blank_' className={styles.iconBtn}>
-                        <FaGithub color="#F5F6F5" size={35} />
+            <div className={styles.footerContainer}>
+                <div className={styles.footerLinks}>
+                    <a href='/'>
+                        <img src={LogoBranca} />
                     </a>
-                    <a href="https://www.linkedin.com/in/khezac" target='blank_' className={styles.iconBtn}>
-                        <FaLinkedin color="#F5F6F5" size={35} />
-                    </a>
+                    <div className={styles.social}>
+                        <a href="https://github.com/Khezac/" target='blank_' className={styles.iconBtn}>
+                            <FaGithub color="#F5F6F5" size={35} />
+                        </a>
+                        <a href="https://www.linkedin.com/in/khezac" target='blank_' className={styles.iconBtn}>
+                            <FaLinkedin color="#F5F6F5" size={35} />
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.footerTexts}>
-                <p>2024 Khezac Khalleb, Todos os Direitos Reservados</p>
-                <p>Projeto OpenSource, acesse o <a className={styles.repoLink} href='https://github.com/Khezac/HumanityClothingCommerce'>código raiz aqui</a></p>
+                <div className={styles.footerTexts}>
+                    <p>2024 Khezac Khalleb, Todos os Direitos Reservados</p>
+                    <p>Projeto OpenSource, acesse o <a className={styles.repoLink} href='https://github.com/Khezac/HumanityClothingCommerce'>código raiz aqui</a></p>
+                </div>
             </div>
         </footer>
     )
