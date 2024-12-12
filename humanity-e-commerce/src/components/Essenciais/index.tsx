5b4cf4d0c48f8849d0ed7cd2560c8b679447287b
Khezac <khezac1@gmail.com>
2024-12-11 22:29:46 -0300

    feat: redirecionamentos da home para a pagina de lista de produtos

--- humanity-e-commerce/src/components/Essenciais/index.tsx
+++ humanity-e-commerce/src/components/Essenciais/index.tsx
@@ -1,14 +1,23 @@
+import { useNavigate } from "react-router"
 import styles from "./styles.module.css"
 
 export const Essenciais = () => {
+    const navigate = useNavigate();
+
     return (
         <section className={styles.essenciaisContainer}>
             <h1>Essenciais</h1>
             <div className={styles.cardsContainer}>
-                <div className={styles.cardMasculino}>
+                <div 
+                    className={styles.cardMasculino}
+                    onClick={() => {navigate('/ProductListPage/masculino')}}
+                >
                     <p>Roupas Masculinas</p>
                 </div>
-                <div className={styles.cardFeminino}>
+                <div 
+                    className={styles.cardFeminino}
+                    onClick={() => {navigate('/ProductListPage/feminino')}}
+                >
                     <p>Roupas Femininas</p>
                 </div>
             </div>
