d730dbdc81bdb80cc7594c0c7690fb5ea2bcd7ae
Khezac <khezac1@gmail.com>
2025-02-15 16:35:03 -0300

    feat: estilização da nova tela de login

--- humanity-e-commerce/src/components/layout/Header/index.tsx
+++ humanity-e-commerce/src/components/layout/Header/index.tsx
@@ -12,23 +12,27 @@ export const Header = () => {
     }
 
     return (
-        <div className={styles.headerContainer}>
-            <Link to="/"><img src={LogoDark}/></Link>
-            <nav>
-                <ul className={styles.menuNav}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><a onClick={() => {goToListPage('masculino')}}>Masculino</a></li>
-                    <li><a onClick={() => {goToListPage('feminino')}}>Feminino</a></li>
-                    <li><a onClick={() => {goToListPage('')}}>Todas as peças</a></li>
-                </ul>
-            </nav>
-            <div className={styles.profileNav}>
-                <div className={styles.profileNavText}>
-                    <p className={styles.welcome}>Seja bem-vindo(a)</p>
-                    <p className={styles.userName}>Cláudio</p> {/* MUDAR FUTURAMENTE PARA O NOME DE QUEM ESTIVER LOGADO */}
+        <>
+        {false &&
+            <div className={styles.headerContainer}>
+                <Link to="/"><img src={LogoDark}/></Link>
+                <nav>
+                    <ul className={styles.menuNav}>
+                        <li><Link to="/">Home</Link></li>
+                        <li><a onClick={() => {goToListPage('masculino')}}>Masculino</a></li>
+                        <li><a onClick={() => {goToListPage('feminino')}}>Feminino</a></li>
+                        <li><a onClick={() => {goToListPage('')}}>Todas as peças</a></li>
+                    </ul>
+                </nav>
+                <div className={styles.profileNav}>
+                    <div className={styles.profileNavText}>
+                        <p className={styles.welcome}>Seja bem-vindo(a)</p>
+                        <p className={styles.userName}>Cláudio</p> {/* MUDAR FUTURAMENTE PARA O NOME DE QUEM ESTIVER LOGADO */}
+                    </div>
+                    <CgProfile color='#56876D' size={60}/>
                 </div>
-                <CgProfile color='#56876D' size={60}/>
             </div>
-        </div>
+        }
+        </>
     )
 }
